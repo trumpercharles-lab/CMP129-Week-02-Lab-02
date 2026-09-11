f0c3c9478debce164f835766594851535569da07
trumpercharles <trumper.charles@student.ccm.edu>
2026-09-11 14:01:58 -0400

    Person Completed

--- Person.java
+++ Person.java
@@ -1,3 +1,32 @@
 public class Person {
-    
+    private String name, email;
+    private int age;
+
+    public void setName(String newName){
+        name=newName;
+    }
+
+    public void setAge(int newAge){
+        age=newAge;
+    } 
+
+    public void setEmail(String newEmail){
+        email=newEmail;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void displayInfo(){
+        System.out.print("Name: "+name+" | Age: "+age+" | Email: "+email);
+    }
 }
